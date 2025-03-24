ea1777a5cdd63f4d388dd11b3d8c25adc1c6054e
agnes <a.tjokrosetio@student.unsw.edu.au>
2025-03-25 00:09:50 +1100

    started working on search page

--- app-frontend/app/(tabs)/search.tsx
+++ app-frontend/app/(tabs)/search.tsx
@@ -1,10 +1,21 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { UNSWFoodList, Categories } from "../../components/UNSWFood";
 
 export default function SearchScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}> Search Screen </Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.heading}>Browse Categories</Text>
+        <View style={styles.cardsList}>
+          {Categories.map((c, index) => (
+            <View key={index} style={styles.card}>
+              <Text style={styles.cardIcon}>{c.icon}</Text>
+              <Text style={styles.cardTitle}>{c.name}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -12,10 +23,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
+    color: "#fff",
+    padding: 20,
+  },
+  row: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    padding: 30,
+    flexDirection: "column",
+    gap: 15,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  cardsList: {
+    flexDirection: "row",
+    gap: 15,
+  },
+  card: {
+    minHeight: 100,
+    minWidth: 150,
+    flexDirection: "column",
+    borderRadius: 20,
+    backgroundColor: "#4F5D75",
     justifyContent: "center",
     alignItems: "center",
+    gap: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 25,
   },
-  text: {
+  cardTitle: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  cardIcon: {
+    fontSize: 36,
     color: "#fff",
   },
 });
---
+++ app-frontend/components/UNSWFood.tsx
@@ -0,0 +1,166 @@
+export const Categories = [
+  {
+    name: "Restaurants",
+    icon: "🍽️",
+  },
+  {
+    name: "Grocery",
+    icon: "🛒",
+  },
+  {
+    name: "Gifts",
+    icon: "🎁",
+  },
+  {
+    name: "Chemist",
+    icon: "💊",
+  },
+];
+
+export const UNSWFoodList = [
+  {
+    name: "Arthouse Kitchen Café",
+    location: "Corner of Oxford Street & Greens Road",
+    image: require("../assets/images/unswfood/arthouse-kitchen.jpg"),
+  },
+  {
+    name: "Boost Jucie",
+    location: "Lower Ground Floor | Morven Brown Building (C20)",
+    image: require("../assets/images/unswfood/boost-juice.jpg"),
+  },
+  {
+    name: "Caffé Brioso",
+    location: "E24a Mathews Arcade & Coffee Cart at Morven Brown",
+    image: require("../assets/images/unswfood/cafe-brioso.jpg"),
+  },
+  {
+    name: "Classic Kebab",
+    location: "Mathews Food Court (F23)",
+    image: require("../assets/images/unswfood/classic-kebab.jpg"),
+  },
+  {
+    name: "Coffee on Campus",
+    location: "Ainsworth Building (J17)",
+    image: require("../assets/images/unswfood/coffee-on-campus.jpeg"),
+  },
+  {
+    name: "Gradueat",
+    location: "Mathews Food Court (F23)",
+    image: require("../assets/images/unswfood/gradueat.jpg"),
+  },
+  {
+    name: "Guzman Y Gomez",
+    location: "University Terraces (B8)",
+    image: require("../assets/images/unswfood/guzman-y-gomez.png"),
+  },
+  {
+    name: "Home Ground Kiosk",
+    location: "Village Green",
+    image: require("../assets/images/unswfood/homeground.jpg"),
+  },
+  {
+    name: "IGA UNSW",
+    location: "University Terraces (B8)",
+    image: require("../assets/images/unswfood/iga.jpeg"),
+  },
+  {
+    name: "Laksa Delight",
+    location: "Mathews Food Court (E24a)",
+    image: require("../assets/images/unswfood/laksa-delight.jpg"),
+  },
+  {
+    name: "Mamak Village",
+    location: "University Terraces (B8)",
+    image: require("../assets/images/unswfood/mamak-village.jpg"),
+  },
+  {
+    name: "NeNe Chicken",
+    location: "Quadrangle Building (E15)",
+    image: require("../assets/images/unswfood/nene-chicken.jpg"),
+  },
+  {
+    name: "PappaRich",
+    location: "Quadrangle Building (E15)",
+    image: require("../assets/images/unswfood/paparich.jpg"),
+  },
+  {
+    name: "Pharmacy @ UNSW",
+    location: "Quadrangle Building (E15)",
+    image: require("../assets/images/unswfood/pharmacy-@-unsw.jpeg"),
+  },
+  {
+    name: "Plume Café",
+    location: "UNSW Bookshop | Quadrangle Building (E15)",
+    image: require("../assets/images/unswfood/plume-cafe.jpg"),
+  },
+  {
+    name: "Sharetea",
+    location: "University Terraces (B8)",
+    image: require("../assets/images/unswfood/sharetea.jpg"),
+  },
+  {
+    name: "Soul Origin",
+    location: "Quadrangle Building (E15)",
+    image: require("../assets/images/unswfood/soul-origin.jpg"),
+  },
+  {
+    name: "Southern Wok",
+    location: "Morven Brown Building (C20)",
+    image: require("../assets/images/unswfood/southern-wok.jpg"),
+  },
+  {
+    name: "Stellini Pasta Bar",
+    location: "University Terraces (B8)",
+    image: require("../assets/images/unswfood/stellini.jpeg"),
+  },
+  {
+    name: "Stockmarket",
+    location: "Mathews Food Court (E24a)",
+    image: require("../assets/images/unswfood/stockmarket.jpeg"),
+  },
+  {
+    name: "Subway",
+    location: "Lower Ground Floor | Morven Brown Building (C20)",
+    image: require("../assets/images/unswfood/subway.jpg"),
+  },
+  {
+    name: "Taste Baguette",
+    location: "Tyree Energy Technologies Building (H6)",
+    image: require("../assets/images/unswfood/taste-baguette.jpg"),
+  },
+  {
+    name: "The Little Marionette",
+    location: "Behind Clancy Auditorium (C24)",
+    image: require("../assets/images/unswfood/little-marionette.jpg"),
+  },
+  {
+    name: "Thirsty Burger & UniBar at The Roundhouse",
+    location: "The Roundhouse (E6)",
+    image: require("../assets/images/unswfood/thirsty-burger.jpg"),
+  },
+  {
+    name: "Thoughtful Foods",
+    location: "Blockhouse (G6)",
+    image: require("../assets/images/unswfood/thoughtful-foods.png"),
+  },
+  {
+    name: "Tori Sushi",
+    location: "Mathews Food Court (E24a)",
+    image: require("../assets/images/unswfood/tori-sushi.jpg"),
+  },
+  {
+    name: "Tropical Green (Pho House)",
+    location: "Mathews Pavilions (E24)",
+    image: require("../assets/images/unswfood/tropical-green.jpg"),
+  },
+  {
+    name: "XS Espresso",
+    location: "Ground Floor | Biological Sciences Building (D26)",
+    image: require("../assets/images/unswfood/xs-espresso.jpg"),
+  },
+  {
+    name: "Yallah Eat Pita, Kebab & Shawarma Bar",
+    location: "University Terraces (B8)",
+    image: require("../assets/images/unswfood/yallah.jpg"),
+  },
+];
