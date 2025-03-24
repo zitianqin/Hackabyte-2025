c8bc101e57d9dfbefa1e379c1c3580f000bbcc6e
Zitian Qin <zitianqin2015@gmail.com>
2025-03-24 17:02:01 +1100

    Initial commit

---
+++ app-frontend/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
+}
