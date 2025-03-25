76b767e63df5887e1c700d435f5e4ac37cd2fe3c
Zitian Qin <zitianqin2015@gmail.com>
2025-03-26 00:52:52 +1100

    Fix website darkMode config build error

--- website-frontend/tailwind.config.ts
+++ website-frontend/tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
+  darkMode: ["class", "dark"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -76,7 +76,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
-
+export default config;
